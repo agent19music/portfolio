26fccbeabbe5f1137c308638a2ebb5125c1fb459
agent19music <seanmotanya@gmail.com>
2024-02-04 12:45:07 +0300

    update navbar

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -32,35 +32,6 @@ export default function Navbar() {
             <li className="nav-item">
               <a className="nav-link active" aria-current="page" href="#">Projects</a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                  <a className="dropdown-item" href="#">Action</a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">Another action</a>
-                </li>
-                <li><hr className="dropdown-divider" /></li>
-                <li>
-                  <a className="dropdown-item" href="#">Something else here</a>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link disabled">Disabled</a>
-            </li>
           </ul>
         </div>
       </div>
