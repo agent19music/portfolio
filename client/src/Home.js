6eb9f044293c6731c1195e0638163a3c5fdf5ca1
agent19music <seanmotanya@gmail.com>
2024-02-04 12:17:54 +0300

    install react-typed library

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import Navbar from './Navbar'
+import {ReactTyped} from "react-typed";
 
 export default function Home() {
   return (
     <div>
     <Navbar/>    
-    Home
+    <h1>
+        {""}
+        <ReactTyped
+          strings={["Hello","Habari","こんにちは","Ng'aki Bono"]}
+          typeSpeed={100}
+          loop
+          backSpeed={20}
+          cursorChar=">"
+          showCursor={true}
+        />
+      </h1>
     </div>
   )
 }
